de8a129f0c522f5741b68ddf8126a8ace32d980a
Colton C Ward <triavanicus@gmail.com>
2020-03-19 03:49:54 -0500

    Make export.py more robust
    How:
    updates modified files
    removes old files
    adds new files
    
    Why:
    it was removing the entire directory, and copying every file every time.
    Now it only updates modified files,
    and removes old ones.

--- export.py
+++ export.py
@@ -3,60 +3,111 @@ import os
 import glob
 import shutil
 
+verbose = False
+
 
 def main():
     name, version = get_name_and_version()
     dir_name = f"{name}_{version}"
-    create_export_directory(dir_name)
-    files = [
-        "changelog.txt",
-        "**/*.json",
-        "**/*.lua",
+    export_folder = f"./exports/{dir_name}/"
+    mods_folder = f"C:/Users/triav/AppData/Roaming/Factorio/mods/{dir_name}/"
+    # TODO rename directory, instead of create a new one, if one exists with
+    # a lower version number
+    create_directory(export_folder)
+    create_directory(mods_folder)
+    export_list = [
         "**/*.png",
-        "locale/**/*.cfg"
+        "**/*.lua",
+        "locale/**/*.cfg",
+        "changelog.txt",
+        "**/*.json"
     ]
-    add_content(dir_name, files)
-    zip_file = create_zip(dir_name)
-    copy_to_mods(f"exports/{dir_name}",
-                 f"C:/Users/triav/AppData/Roaming/Factorio/mods/{dir_name}")
+    mod_files = get_files(export_list)
+    update_mod_files(export_folder, mods_folder, mod_files)
+    remove_extra_files(export_folder, mods_folder, export_list)
+    create_zip(export_folder, dir_name)
 
 
 def get_name_and_version():
-    with open("./info.json", "r") as file:
-        obj = json.load(file)
-        return obj["name"], obj["version"]
+    filename = "./info.json"
+    try:
+        with open(filename, "r") as file:
+            obj = json.load(file)
+            return obj["name"], obj["version"]
+    except FileNotFoundError as file_error:
+        print(f"File was not found: {file_error.filename}")
+        exit(1)
+    except KeyError as key_error:
+        print(f"'{filename}' does not have key '{key_error.args[0]}'")
+        exit(2)
 
 
-def create_export_directory(name):
-    if not os.path.exists("exports"):
-        os.mkdir("exports")
-    if not os.path.exists(f"exports/{name}"):
-        os.mkdir(f"exports/{name}")
+def create_directory(directory):
+    directories = directory.split("/")
+    for i in range(1, len(directories)):
+        name = "/".join(directories[0:i])
+        if not os.path.exists(name):
+            if verbose:
+                print(f"creating directory \"{name}\"")
+            os.mkdir(name)
 
 
-def create_zip(name):
-    zip_name = f"exports/{name}"
-    return shutil.make_archive(zip_name, "zip", f"exports/", name)
-
-
-def add_content(dir_name, files):
-    for i in files:
-        for filename in glob.iglob(i, recursive=True):
+def get_files(file_list, base_dir="./"):
+    for i in file_list:
+        name = f"{base_dir}{i}"
+        for filename in glob.iglob(name, recursive=True):
             filename = "/".join(filename.split("\\"))
-            if filename.startswith("exports/"):
+            if filename.startswith("./exports/"):
                 continue
-            directories = filename.split("/")
-            for i in range(1, len(directories)):
-                name = f'exports/{dir_name}/{"/".join(directories[0:i])}'
-                if not os.path.exists(name):
-                    os.mkdir(name)
-            shutil.copy2(filename, f"exports/{dir_name}/{filename}")
+            yield filename
 
 
-def copy_to_mods(src, dst):
-    if os.path.exists(dst):
-        shutil.rmtree(dst)
-    shutil.copytree(src, dst, )
+def update_mod_files(export_folder, mods_folder, files):
+    for file in files:
+        export_file = f"{export_folder}{file}"
+        mods_file = f"{mods_folder}{file}"
+        create_directory(export_file)
+        create_directory(mods_file)
+        update_file(file, export_file)
+        update_file(file, mods_file)
+
+
+def update_file(src, dest):
+    if os.path.exists(dest):
+        if os.path.getmtime(src) == os.path.getmtime(dest):
+            if verbose:
+                print(f"skipping \"{dest}\"")
+        else:
+            if verbose:
+                print(f"updating \"{dest}\"")
+            shutil.copy2(src, dest)
+    else:
+        if verbose:
+            print(f"adding \"{dest}\"")
+        shutil.copy2(src, dest)
+
+
+def remove_extra_files(export_folder, mods_folder, export_list):
+    file_list = get_files(export_list, mods_folder)
+    for file in file_list:
+        rel_file = file[len(mods_folder):]
+        export_file = f"{export_folder}{rel_file}"
+        if not os.path.exists(export_file):
+            if verbose:
+                print(f"removing \"{file}\"")
+            os.remove(file)
+            rel_folders = rel_file.split("/")[:-1]
+            folders = (f'{mods_folder}{"/".join(rel_folders[0:i])}'
+                       for i in range(len(rel_folders), -1, -1))
+            for folder in folders:
+                if os.path.exists(folder) and os.path.isdir(folder) and not os.listdir(folder):
+                    if verbose:
+                        print(f"removing \"{folder}\"")
+                    os.rmdir(folder)
+
+
+def create_zip(dest, dir_name):
+    return shutil.make_archive(dest, "zip", "exports/", dir_name)
 
 
 if __name__ == "__main__":
